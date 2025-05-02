cb2931fd2da47b2bd3dd7532404f95af812933f8
andres polo <andresepb.30@gmail.com>
2025-05-02 18:37:25 -0500

    pruebas de error

--- js/helpers/viewCard.js
+++ js/helpers/viewCard.js
@@ -35,6 +35,7 @@ export function crearCardRuletas(container) {
             element.addEventListener("click", (e) => {
                 let idRuleta = e.target.parentElement.parentElement.parentElement.id
                 let ruletaBuscada = ruletas.find((element) => element.id == idRuleta)
+                console.log(ruletaBuscada)
                 localStorage.setItem("RuletaSeleccionada", JSON.stringify(ruletaBuscada))
                 location.href = "ruleta.html";
             })
